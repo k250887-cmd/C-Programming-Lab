50b2594ff38f039282180f4604b9fb0bdb16bb78
k250887-cmd <k250887@nu.edu.pk>
2025-09-28 07:10:07 -0700

    Add files via upload

---
+++ LAB 5 TASK 8.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter an integer:");
+	scanf("%d",&a);
+	printf("Enter an integer:");
+	scanf("%d",&b);
+	if(a==b)
+	printf("true");
+	else 
+	printf("false\n");
+	if(a>b)
+	printf("true\n");
+	else 
+	printf("false\n");
+	if(a<b)
+	printf("true\n");
+	else 
+	printf("false\n");
+	if(a>=b)
+	printf("true\n");
+	else 
+	printf("false\n");
+	if(a<=b)
+	printf("true\n");
+	else 
+	printf("false\n");
+	if(a!=b)
+	printf("true\n");
+	else 
+	printf("false\n");
+}
+	
